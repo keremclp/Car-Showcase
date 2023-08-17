049729158239c0533bf0b983e2bb6fc565daea97
KEREM CAN ÇELEPKOLU <kerem00962@gmail.com>
2023-08-17 11:57:10 +0300

    CarCard components and calculateCarRent function

--- components/index.ts
+++ components/index.ts
@@ -6,7 +6,7 @@ import SearchBar from "./SearchBar";
 import CustomFilter from "./CustomFilter";
 import SearchManufacturer from "./SearchManufacturer";
 import CarCard from "./CarCard";
-
+import CardDetails from "./CardDetails";
 export { 
     Hero ,
     CustomButton,
@@ -15,5 +15,6 @@ export {
     SearchBar,
     CustomFilter,
     SearchManufacturer,
-    CarCard
+    CarCard,
+    CardDetails
 };
